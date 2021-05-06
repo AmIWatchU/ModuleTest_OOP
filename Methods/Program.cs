7cf19daae98b6034aa7510e690609604750fee5d
AmIWatchU <user106512@gmail.com>
2021-05-06 15:08:07 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        //Розробити методи для знаходження мінімального, максимального та середнього
+        //значень, а також суми елементів одновимірного числового масиву.Продемонструвати
+        //роботу цих методів.
+
+        //Метод для знаходження для знаходження мінімального значення
+        static void Min(int[] arr)
+        {
+            Array.Sort(arr);
+            Console.WriteLine("Мiнiмальне значення масиву: ");
+            Console.WriteLine(arr[0]);
+        }
+        //Метод для знаходження для знаходження максимального значення
+        static void Max(int[] arr)
+        {
+            Array.Sort(arr);
+            var last = arr[arr.Length - 1];
+            Console.WriteLine("Максимальне значення масиву: ");
+            Console.WriteLine(last);
+        }
+        //Метод для знаходження для знаходження середнього значення
+        static void Avr(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            Console.WriteLine("Середнє значення масиву: ");
+            Console.WriteLine("{0:0.##}", sum / arr.Length);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Масив: ");
+            int[] a = new[] { 2, 4, 6, 8, 22, 84, 64, 45, -24 };
+            foreach (var b in a)
+            {
+                Console.Write(b + " ");
+            }
+            Console.Write("\n");
+            Min(a);
+            Max(a);
+            Avr(a);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ModuleTest_OOP/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTest_OOP
+{
+
+    //   Створити двовимірний масив випадкових чисел заданого діапазону та виконати завдання згідно варіанту
+    //    Розмірність масиву: 6x8
+    //    Діапазон значeнь: 10-99
+    //    Завдання:
+    //    Знайти середнє значення в кожному рядку матриці.
+    //    Поміняти місцями рядок, який містить перше входження
+    //    мінімального середнього значення з рядком, який містить
+    //    останнє входження максимального середнього значення
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int m = 6;
+            const int n = 8;
+            const int min = 10;
+            const int max = 99;
+            var arr = new int[m,n];
+            var rand = new Random();
+            // Оголошення масиву
+            for (var i = 0; i < m; i++)
+                for (var j = 0; j < n; j++)
+                    arr[i, j] = rand.Next(min, max + 1);
+            // Вивід масиву
+            Console.WriteLine("Масив :");
+            for (int i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    Console.Write("{0,3}", arr[i, j]);
+            // Знаходження середнього арифметичного
+            var avg = new double[m];
+            for (int i = 0; i < m; i++)
+            {
+                var sum = 0.0d;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += arr[i, j];
+                }
+                avg[i] = sum / n;
+
+                //foreach (var a in avg[i])
+                //{
+                //    Console.WriteLine(a);
+                //}
+            }
+
+            var indexMin = 0;
+            var indexMax = 0;
+            var avgMin = 100d;
+            var avgMax = -100d;
+            //Знаходження індексів рядків з максимальним і мінімальним середнім значенням
+            for (int i = 0; i < m; i++)
+            {
+                if (avg[i] < avgMin) //Перший індекс серед мінімальних
+                {
+                    indexMin = i;
+                    avgMin = avg[i];
+                   
+                }
+                if (avg[i] >= avgMax) //Останній індекс серед максимальних
+                {
+                    indexMax = i;
+                    avgMax = avg[i];
+                }
+
+            }
+            //Заміна значень рядків між собою
+            for (int i = 0; i < n; i++)
+            {
+                var tmp = arr[indexMin, i];
+                arr[indexMin, i] = arr[indexMax,i];
+                arr[indexMax, i] = tmp;
+            }
+            //Вивід массиву з заміненими рядками
+            Console.WriteLine("Оновлений масив c заміненими рядками:");
+            for (int i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    Console.Write("{0,3}", arr[i, j]);
+
+        }
+    }
+}
